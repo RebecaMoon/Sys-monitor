e393ce55b5ffbd885b4e079da29f18bf996b9fc6
Oscar Cuevas <oscar-aldair12@hotmail.com>
2026-05-20 13:52:32 -0600

    feat(agent): implement http push functionality for metrics

--- agent/agent.py
+++ agent/agent.py
@@ -1,11 +1,15 @@
 import psutil
 import time
+import requests
+
+# Define the future API URL
+API_URL = "http://localhost:8000/metrics"
 
 def get_system_metrics():
     #Collects CPU, Memory, and Disk usage.
     
     metrics = {
-        # Save the data in json format.
+        # Save the data in a dictionary 
         "cpu_percent": psutil.cpu_percent(interval=1),
         "memory_percent": psutil.virtual_memory().percent,
         "disk_percent": psutil.disk_usage('/').percent,
@@ -23,7 +27,17 @@ def run_agent():
             print(f"Memory: {data['memory_percent']}%")
             print(f"Disk: {data['disk_percent']}")
             print(f"Time: {data['timestamp']}")
-            print("------------------------------")
+            
+
+            # Send the metrics to the API.
+            try:
+            
+                response = requests.post(API_URL, json=data, timeout=5) # With the timeout 5 the agent doesn't hang if the API doesn't answer
+                print(f"Sent data! Status Code: {response.status_code}")
+                print("------------------------------")
+            except requests.exceptions.ConnectionError:
+                print("Failed to send the metrics")
+                print("------------------------------")
             
             # Wait 5 seconds before next collection
             time.sleep(5)
