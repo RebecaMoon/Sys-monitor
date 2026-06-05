ae210d2c4822484433314cde1978c0721c4cb487
Oscar Cuevas <oscar-aldair12@hotmail.com>
2026-06-05 14:50:36 -0600

    chore(config): move database credentials to environment variables

--- api_python/database.py
+++ api_python/database.py
@@ -1,15 +1,20 @@
+import os
+from dotenv import load_dotenv
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
 
+load_dotenv() # Load env variables from .env
 
-DATABASE_URL = "postgresql://reb_admin:Z4mperf1@localhost:5432/monitor_db" # Database connection URL.
 
- 
+DATABASE_URL = os.getenv("DATABASE_URL")# Read the database URL from the .env
+
+if DATABASE_URL is None:
+    raise ValueError("DATABASE_URL is not set in the environment variables")
+
 engine = create_engine(DATABASE_URL) # Creates the SQLAlchemy engine used to connect to the database.
 
-
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine) # Creates a factory for database sessions.
-
 
 Base = declarative_base() # Creates the base class used to define database tables.
