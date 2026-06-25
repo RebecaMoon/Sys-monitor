54f9f3a4a47af39f175c144fd1c6f7d6d11d592e
Oscar Cuevas <oscar-aldair12@hotmail.com>
2026-06-25 17:29:26 -0600

    feat(api-go): add database models and connection

---
+++ api_go/models.go
@@ -0,0 +1,8 @@
+package main
+
+type Metric struct { // This defines the structure of the data we receive from the agent.
+	CPUPercent    float64 `json:"cpu_percent"`    // The fields start with uppercase letters because in Go, if a field starts with an uppercase letter, it is exported.
+	MemoryPercent float64 `json:"memory_percent"` // That means other packages can access it, such as the encoding/json package in this case.
+	DiskPercent   float64 `json:"disk_percent"`
+	Timestamp     float64 `json:"timestamp"`
+}
