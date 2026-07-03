e76e5d006dbfeb123d1e434beae1d70b06821efa
Oscar Cuevas <oscar-aldair12@hotmail.com>
2026-07-03 15:16:07 -0600

    feat(frontend): add basic UI design

---
+++ frontend/src/components/MetricsGraph.jsx
@@ -0,0 +1,13 @@
+function MetricsGraph() {
+  return (
+    <section className="metrics-graph">
+      <h2>Graphic</h2>
+
+      <div className="graphic-placeholder">
+        Graphic will be displayed here
+      </div>
+    </section>
+  )
+}
+
+export default MetricsGraph
